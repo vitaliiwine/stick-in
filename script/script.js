0570a6a7a98ce7cca01d56dc3e6c6d83f5527d17
Vitalii <47257917+vitaliiwine@users.noreply.github.com>
2019-05-05 16:09:56 +0300

    final v

--- script/script.js
+++ script/script.js
@@ -1,4 +1,32 @@
-
 // Get the current year
 
 $('#year').text(new Date().getFullYear());
+
+// Init scroll spy
+
+$('body').scrollspy({target: '#main-nav'});
+
+// Smooth scrolling
+
+$("#main-nav a").on('click', function (event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function () {
+            window.location.hash = hash;
+        });
+    }
+
+});
+
+$(document).scroll(function () {
+    console.log("kek")
+    if ($(document).scrollTop() > 100)
+        $("#main-nav").removeClass("swag")
+    else
+        $("#main-nav").addClass("swag")
+});
