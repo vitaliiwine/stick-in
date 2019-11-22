0d4663203d385abee523e2b29ae43c7fcc774980
wine <wineeewineee@gmail.com>
2019-11-22 20:25:06 +0300

    update

--- script/script.js
+++ script/script.js
@@ -1,32 +1,31 @@
-// Get the current year
-
-$('#year').text(new Date().getFullYear());
-
-// Init scroll spy
-
-$('body').scrollspy({target: '#main-nav'});
-
-// Smooth scrolling
-
-$("#main-nav a").on('click', function (event) {
-    if (this.hash !== "") {
-        event.preventDefault();
-
-        const hash = this.hash;
-
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 800, function () {
-            window.location.hash = hash;
-        });
-    }
-
-});
-
-$(document).scroll(function () {
-    console.log("kek")
-    if ($(document).scrollTop() > 100)
-        $("#main-nav").removeClass("swag")
-    else
-        $("#main-nav").addClass("swag")
-});
+// // Get the current year
+//
+// $('#year').text(new Date().getFullYear());
+//
+// // Init scroll spy
+//
+// $('body').scrollspy({target: '#main-nav'});
+//
+// // Smooth scrolling
+//
+// $("#main-nav a").on('click', function (event) {
+//     if (this.hash !== "") {
+//         event.preventDefault();
+//
+//         const hash = this.hash;
+//
+//         $('html, body').focus()animate(yyyy{
+//             scrollTop: $(hash).offset().top
+//         }, 800, function () {
+//             window.location.hash = hash;
+//         });
+//     }
+//
+// });
+//
+// $(document).scroll(function () {
+//     if ($(document).scrollTop() > 100)
+//         $("#main-nav").removeClass("color-change")
+//     else
+//         $("#main-nav").addClass("color-change")
+// });
